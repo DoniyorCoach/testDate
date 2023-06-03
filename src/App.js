8cd55d61dc3b719066fdc84005a7def4c70ccd77
doniyor <doniyorcoach@gmail.com>
2023-06-03 21:20:53 +0500

    fwfwe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import moment from 'moment/moment';
 
 import 'moment/locale/ru';
@@ -6,13 +6,17 @@ import 'moment/locale/ru';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState('2023-06-03 01:10:39.676358+05');
+  const [date, setDate] = useState('');
+
+  useEffect(() => {
+    setDate('2023-06-03 01:10:39.676358+05');
+  }, []);
 
   const formatDate = (date) => {
     return moment(date).format('D MMMM YYYY, HH:mm');
   };
 
-  return <>created_at: {formatDate(date)}</>;
+  return <>{date && <div>created_at: {formatDate(date)}</div>}</>;
 }
 
 export default App;
